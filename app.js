8bf361f644656b3e3223caa6c3c8ac1d34d95134
camilin87 <camilin87@gmail.com>
2016-02-20 17:34:26 -0500

    create a proxy api

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ app.get('/', function (req, res) {
 app.listen(app.get('port'), function () {
   console.log('Karma trends listening on port ' + app.get('port'));
 });
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/api/search', function(req, res){
+    console.log("q: ", req.body.q);
+    result = {
+        q: req.body.q
+    };
+    res.send(result);
+});
