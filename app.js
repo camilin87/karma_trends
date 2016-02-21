e1bb0ba1317feedd6696f2ed84281a78f4c68d62
camilin87 <camilin87@gmail.com>
2016-02-20 20:32:21 -0500

    cleanup the app a lil bit

--- app.js
+++ app.js
@@ -19,32 +19,4 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var apiSearch = require('./controllers/api-search.js');
-app.post('/api/search', apiSearch);
-// var Twitter = require('twitter-node-client').Twitter;
-// var parseTweets = require('./services/parse-tweets.js');
-// app.post('/api/search', function(req, res){
-//     console.log("q:", req.body.q, "geocode:", req.body.geocode);
-
-//     var config = require("./secrets/twitter.json");
-//     var twitter = new Twitter(config);
-
-//     var apiQuery = {
-//         'q':req.body.q,
-//         'geocode': req.body.geocode,
-//         'lang': 'en',
-//         'count': 1000
-//     };
-//     twitter.getSearch(
-//         apiQuery,
-//         function (err, response, body) {
-//             console.log('ERROR [%s]', err);
-//             res.status(500);
-//             res.send("api error");
-//         },
-//         function (data) {
-//             var tweetsData = parseTweets(data);
-//             res.send(tweetsData);
-//         }
-//     );
-// });
+app.post('/api/search', require('./controllers/api-search.js'));
