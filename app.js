18a12bbcb19d8c9ea03f70533c12f08cfee84940
camilin87 <camilin87@gmail.com>
2016-02-20 21:54:41 -0500

    refactor app.js display the api search page by default

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.set('port', process.env.PORT || 3000);
 
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(app.get('port'), function () {
-  console.log('Karma trends listening on port ' + app.get('port'));
-});
-
-var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('Karma trends listening on port ' + app.get('port'));
+});
 
 app.post('/api/search', require('./controllers/api-search.js'));
